98d7eea562e62033aa15ba37afd3bc8c83a6513c
BennyBlanco88 <ben.langs@yahoo.com>
2023-11-03 14:31:26 -0700

    added nav bar functionality

--- assets/display-search.js
+++ assets/display-search.js
@@ -11,6 +11,7 @@ var searchTerm = urlParam.get('q')
 
 
 
+
 // EVENT LISTENER    <--- on load for now, until we connect functionality from index 
 
 window.addEventListener("load", (event) => {
@@ -197,4 +198,25 @@ function displayRecipes(hits) {
   container.appendChild(cardContainer);
 }
 
+//nav bar functionality
+document.addEventListener('DOMContentLoaded', () => {
 
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,42 @@ var searchInput = document.querySelector('#search-query');
 var resultsList = document.querySelector('#results');
 var saveBtn = document.createElement('button');
 
+
+//     //Nav bar functionality
+// var navLinkEl = document.querySelectorAll('.navbar-item');
+// var windowPathname = window.location.pathname;
+
+// // navLinkEl.forEach(navLinkEl => {
+// //   const navLinkPathname = new URL(navLinkEl.href).pathname;
+
+// //   if (windowPathname === navLinkPathname) {
+// //     navLinkEl.classList.add('active')
+// //   }
+// // })
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
+
+
+
 searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
   var searchValue = searchInput.value.trim();
@@ -345,3 +381,5 @@ fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q
         console.log(curr)
       })
     })
+
+
